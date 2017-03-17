cdb4791025ac960f4d166e3265249c4ccee3407d
Dave Puchyr <dave@avaritia.com>
2017-03-17 23:10:57 +0100

    Revamped ctci-find-the-running-median.cpp since version 1 timed out.

--- c++/hackerrank/ctci-find-the-running-median.cpp
+++ c++/hackerrank/ctci-find-the-running-median.cpp
@@ -7,29 +7,79 @@ using namespace std;
 
 
 int main() {
-   int i, n;
-   multiset<int> a;
- 
+   multiset<int> hi;
+   multiset<int, greater<int>> lo;
+
    cout << fixed << setprecision( 1 );
-   cin >> n;
 
-   for ( int a_i = 0; a_i < n; ++a_i ) {
-      cin >> i;
-      a.insert( i );
+   auto print = [&hi, &lo]( int i ) {
+      float median = *hi.begin();
 
-      auto it = a.begin();
-      auto m = a_i / 2;
-      
-      advance( it, m );
-
-      float median = *it;
-
-      if ( a_i % 2 ) {
-         advance( it, 1 );
-         median = 0.5 * ( median + *it );
-      }
+      if ( i & 1 ) median = 0.5 * ( median + *lo.begin() );
 
       cout << median << endl;
+   };
+
+   auto hi2lo = [&hi, &lo]() {
+      auto it = hi.begin();
+      lo.insert( *it );
+      hi.erase( it );
+   };
+
+   auto lo2hi = [&hi, &lo]() {
+      auto it = lo.begin();
+      hi.insert( *it );
+      lo.erase( it );
+   };
+
+   int x, n, i = 0;
+
+   cin >> n;
+
+   if ( n < 1 ) return 0;
+
+   cin >> x;
+
+   hi.insert( x );
+
+   print( i++ );
+
+   if ( n < 2 ) return 0;
+
+   cin >> x;
+
+   if ( x < *hi.begin() ) {
+      lo.insert( x );
+   } else {
+      hi2lo();
+      hi.insert( x );
+   }
+
+   print( i++ );
+
+   while ( i < n ) {
+      cin >> x;
+
+      auto nhi = hi.size(), nlo = lo.size();
+      auto hi0 = *hi.begin(), lo0 = *lo.begin();
+
+      if ( x < lo0 ) {
+         if ( nlo == nhi ) lo2hi();
+
+         lo.insert( x );
+      } else if ( x > hi0 ) {
+         if ( nhi > nlo ) hi2lo();
+
+         hi.insert( x );
+      } else {
+         if ( nlo < nhi ) {
+            lo.insert( x );
+         } else {
+            hi.insert( x );
+         }
+      }
+
+      print( i++ );
    }
 
    return 0;
